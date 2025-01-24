291f49cf927aab175cabe2bad62a698309708c0f
dwssef <dwssef@protonmail.com>
2025-01-24 10:42:03 +0800

    update conda mirror to use NJU

--- setup_conda.sh
+++ setup_conda.sh
@@ -23,21 +23,10 @@ conda config --set show_channel_urls yes
 echo "Writing to ~/.condarc..."
 cat << EOF > ~/.condarc
 channels:
+  - https://mirror.nju.edu.cn/anaconda/cloud/bioconda/
+  - https://mirror.nju.edu.cn/anaconda/cloud/conda-forge/
+  - https://mirror.nju.edu.cn/anaconda/pkgs/main/
   - defaults
-show_channel_urls: true
-default_channels:
-  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main
-  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/r
-  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/msys2
-custom_channels:
-  conda-forge: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
-  msys2: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
-  bioconda: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
-  menpo: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
-  pytorch: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
-  pytorch-lts: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
-  simpleitk: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
-  deepmodeling: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
 EOF
 
 # 设置 pip 源
