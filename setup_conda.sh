3497eb056342c941b4a2dd4d4e26ae6958309bd5
dwssef <39583993+dwssef@users.noreply.github.com>
2025-03-04 16:48:41 +0800

    fix: Support for installing Conda on aarch64 systems

--- setup_conda.sh
+++ setup_conda.sh
@@ -3,14 +3,28 @@
 if command -v conda &>/dev/null; then
     echo "conda installed"
     exit 0
-else
-    wget https://repo.anaconda.com/miniconda/Miniconda3-py311_24.9.2-0-Linux-x86_64.sh -O Miniconda3.sh
-    bash Miniconda3.sh -b -p $HOME/miniconda3
-    source $HOME/miniconda3/etc/profile.d/conda.sh
-    conda init
-    source ~/.bashrc
-    echo "Miniconda installation complete."
 fi
+
+ARCH=$(uname -m)
+if [[ "$ARCH" == "aarch64" ]]; then
+    MINICONDA_URL="https://repo.anaconda.com/miniconda/Miniconda3-py311_24.11.1-0-Linux-aarch64.sh"
+else
+    MINICONDA_URL="https://repo.anaconda.com/miniconda/Miniconda3-py311_24.9.2-0-Linux-x86_64.sh"
+fi
+
+if [[ -f "Miniconda3.sh" ]]; then
+    echo "Using existing Miniconda3.sh"
+else
+    echo "Downloading Miniconda from $MINICONDA_URL"
+    wget "$MINICONDA_URL" -O Miniconda3.sh
+fi
+
+bash Miniconda3.sh -b -p "$HOME/miniconda3"
+source "$HOME/miniconda3/etc/profile.d/conda.sh"
+conda init
+source ~/.bashrc
+
+echo "Miniconda installation complete."
 
 echo "Activating base conda environment..."
 conda activate base
@@ -18,9 +32,6 @@ conda activate base
 echo "Setting conda show_channel_urls to yes..."
 conda config --set show_channel_urls yes
 
-# 写入 ~/.condarc
-# 将conda源 和 pip源设置为清华源（用于加速）
-echo "Writing to ~/.condarc..."
 cat << EOF > ~/.condarc
 channels:
   - https://mirror.nju.edu.cn/anaconda/cloud/bioconda/
@@ -29,9 +40,7 @@ channels:
   - defaults
 EOF
 
-# 设置 pip 源
 echo "Setting pip index-url to https://pypi.tuna.tsinghua.edu.cn/simple..."
 pip config set global.index-url https://mirrors.ustc.edu.cn/pypi/simple/
 
 echo "Configuration completed."
-
